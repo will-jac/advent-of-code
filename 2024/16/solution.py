38bceab75e88f2354b3a21ee20ee338fee142a66
Jack Williams <jacwilli@epic.com>
2024-12-16 17:33:20 -0600

    uhh pt 2

--- 2024/16/solution.py
+++ 2024/16/solution.py
@@ -12,35 +12,57 @@ for p,v in M.items():
         break
 d = 1
 
-def djikstra(p,d,c):
-    Q = [(p,d,c)]
+def djikstra(c,p,d):
+    Q = [(c,p,d,[p])]
     visiting = {}
     processed = set()
     iter = 0
+    best_cost = None
+    best_paths = []
     while len(Q) > 0:
-        p,d,c = Q.pop(0)
+        c,p,d,P = Q.pop(0)
         if (p,d) in processed:
             continue
         processed.add((p,d))
 
-        if M[p] == 'E': return c
+        if best_cost is not None and c > best_cost:
+            return best_cost, best_paths
 
+        if M[p] == 'E': 
+            best_cost = c
+            best_paths.append(P)
+            continue
+        
         # keep walking
         if p+d in M and M[p+d] != '#':
-            if (p+d,d,c+1) not in visiting or visiting[(p+d, d)] > c+1: 
-                visiting[(p+d, d)] = c+1
-                bisect.insort_left(Q, (p+d,d,c+1), key=lambda x: x[2])
+            cc = c+1
+            pp = p+d
+            dd = d
+            if (cc,pp,dd) not in visiting or visiting[(pp, dd)] > cc: 
+                PP = P.copy()
+                PP.append(pp)
+                visiting[(pp, dd)] = cc
+                bisect.insort_left(Q, (cc,pp,dd,PP), key=lambda x: x[0])
         # turn and move one step
         for dd in [d*1j, d*-1j]:
-            if p+dd in M and M[p+dd] != '#':
-                if (p+dd,dd,c+1001) not in visiting or visiting[(p+dd, dd)] > c+1001: 
-                    visiting[(p+dd, dd)] = c+1001
-                    bisect.insort_left(Q, (p+dd,dd,c+1001), key=lambda x: x[2])
+            cc = c+1001
+            pp = p+dd
+            if pp in M and M[pp] != '#':
+                if (cc,pp,dd) not in visiting or visiting[(pp, dd)] > cc: 
+                    PP = P.copy()
+                    PP.append(pp)
+                    visiting[(pp, dd)] = cc
+                    bisect.insort_left(Q, (cc,pp,dd,PP), key=lambda x: x[0])
 
         # print(p,d,c, '\t', len(Q), Q[:5])
         iter += 1
         if iter % 1000 == 0:
             print(iter, len(Q), len(visiting), len(processed))
 
-res = djikstra(p,d,0)
-print(res)
+c, P = djikstra(0,p,d)
+print(c)
+seats = set()
+for path in P:
+    for p in path:
+        seats.add(p)
+print(len(seats))
